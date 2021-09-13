d78a35cd839414939726dc8ae9f1fb88284b02ed
jiyuujin <jiyuujinunite@gmail.com>
2021-09-13 15:43:42 +0900

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,36 +1,17 @@
 import * as React from 'react'
-import format from 'date-fns/format'
 import styles from './footer.module.scss'
 
-async function fetchLastUpdated(setLastUpdated: React.Dispatch<React.SetStateAction<string>>) {
-  const data = await fetch('https://api.github.com/repos/nekohack/nekohack.me')
-    .then((v) => v.json())
-    .catch(() => null)
-
-  if (!data) return null
-
-  if (data.updated_at) {
-    const formatted = format(new Date(data.updated_at), 'MMMM dd, yyyy HH:mm')
-    setLastUpdated(formatted)
-  }
-}
-
 export const Footer = () => {
-  const [lastUpdated, setLastUpdated] = React.useState<string | null>(null)
-
-  React.useEffect(() => {
-    fetchLastUpdated(setLastUpdated)
-  }, [])
-
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <p>
-          Created by <a href="https://github.com/nekohack/nekohack.me">Portfolio</a> with ❤️
+          At once read <a href="https://nekohack-privacy-policy.netlify.app">Privacy Policy</a>
           <br />
-          Colors: Dark mode. Built with <a href="https://nextjs.org">Next.js</a>
-          <br />
-          {lastUpdated ? <>Last updated: {lastUpdated}</> : null}
+          Please contact easily from{' '}
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFzwsCVnFbu-lV0Jz2fEYHR97hpBSK2g6kEwc-G1fo976ngA/viewform">
+            Survey Form
+          </a>
         </p>
 
         <div className={styles.footerLinks}>
