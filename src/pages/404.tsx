b70a1cd2e59b31219997970256e12cae335c6822
jiyuujin <jiyuujinunite@gmail.com>
2021-09-26 19:29:20 +0900

    delete unused styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,21 @@
 import Head from 'next/head'
-import styles from 'css/404.module.scss'
+import mainStyles from 'css/main.module.scss'
 
 const Custom404 = () => {
   return (
-    <>
+    <main>
       <Head>
         <title>Page Not Found | 404 - nekohack.me</title>
         <meta property="og:title" content="Page Not Found - Casper Iversen" />
         <link rel="canonical" href="https://nekohack.me/404" />
       </Head>
-      <div className={styles.container404}>
+      <div className={mainStyles.mainTitle}>
         <h1>404</h1>
+      </div>
+      <div className={mainStyles.mainText}>
         <p>Page Not Found</p>
       </div>
-    </>
+    </main>
   )
 }
 
