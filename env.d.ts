468d9af09f980a0378891395b54cb00bad5d7786
jiyuujin <jiyuujinunite@gmail.com>
2021-09-27 18:53:18 +0900

    delete environment variables

--- env.d.ts
+++ env.d.ts
@@ -2,10 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_GITHUB_PROFILE_URL: string
     NEXT_PUBLIC_TWITTER_PROFILE_URL: string
-    NEXT_PUBLIC_YOUTUBE_REDIRECT_URL: string
     NEXT_PUBLIC_CONTACT_URL: string
-    NEXT_PUBLIC_NPM_URL: string
-    TO_EMAIL: string
-    SEND_GRID_API_KEY: string
   }
 }
