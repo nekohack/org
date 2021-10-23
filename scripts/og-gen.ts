ee295fca3b304d452cb27f2f8e27b23cd92317e8
jiyuujin <jiyuujinunite@gmail.com>
2021-10-24 08:40:00 +0900

    feat: og meta

--- scripts/og-gen.ts
+++ scripts/og-gen.ts
@@ -1,15 +1,31 @@
 const puppeteer = require('puppeteer')
 const path = require('path')
 
+const fs = require('fs')
+
+const OG_SOURCE_DIR_PATH = path.join(__dirname, '..', 'data')
+const OG_SOURCE_HTML_FILE_PATH = path.join(OG_SOURCE_DIR_PATH, 'og.html')
 const OG_DIR_PATH = path.join(__dirname, '..', 'public', 'og')
 
-async function captureOgImage(browser: any, url: string, imagePath: string) {
+async function captureOgImage(
+  browser: any,
+  url: string = '',
+  title: string = '',
+  description: string = '',
+  imagePath: string,
+) {
   const page = await browser.newPage()
   await page.setViewport({
     width: 1200,
     height: 630,
   })
-  await page.goto(url)
+  if (url) {
+    await page.goto(url)
+  } else {
+    await page.goto('file://' + OG_SOURCE_HTML_FILE_PATH)
+    await page.exposeFunction('getTitle', () => title)
+    await page.exposeFunction('getDescription', () => description)
+  }
   await page.reload()
   await page.screenshot({
     path: imagePath,
@@ -20,18 +36,29 @@ async function captureOgImage(browser: any, url: string, imagePath: string) {
 
 ;(async function () {
   const ogList = [
-    { title: 'webneko-blog', url: 'https://webneko.dev' },
-    { title: 'nekohack-me', url: 'https://nekohack.me' },
-    { title: 'yuma_kitamura-nekohack-me', url: 'https://yuma-kitamura.nekohack.me' },
-    { title: 'flutterkaigi-2021', url: 'https://flutterkaigi.jp' },
-    { title: 'vscodejpcon-2021', url: 'https://vscodejp.github.io/conf2021/ja' },
+    { slug: 'top', url: '', title: 'Yuma Kitamura', description: 'Web Developer' },
+    { slug: 'webneko-blog', url: 'https://webneko.dev', title: '', description: '' },
+    { slug: 'nekohack-me', url: 'https://nekohack.me', title: '', description: '' },
+    {
+      slug: 'yuma_kitamura-nekohack-me',
+      url: 'https://yuma-kitamura.nekohack.me',
+      title: '',
+      description: '',
+    },
+    { slug: 'flutterkaigi-2021', url: 'https://flutterkaigi.jp', title: '', description: '' },
+    {
+      slug: 'vscodejpcon-2021',
+      url: 'https://vscodejp.github.io/conf2021/ja',
+      title: '',
+      description: '',
+    },
   ]
 
   const browser = await puppeteer.launch()
 
-  for (const { title, url } of ogList) {
-    const OG_IMAGE_FILE_PATH = `${OG_DIR_PATH}/` + title + '.jpg'
-    await captureOgImage(browser, url, OG_IMAGE_FILE_PATH)
+  for (const { slug, url, title, description } of ogList) {
+    const OG_IMAGE_FILE_PATH = `${OG_DIR_PATH}/` + slug + '.jpg'
+    await captureOgImage(browser, url, title, description, OG_IMAGE_FILE_PATH)
   }
 
   await browser.close()
