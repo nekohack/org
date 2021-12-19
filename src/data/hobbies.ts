0216f0fba4248cf3fbe9ec2212f3115b9a2185b3
jiyuujin <jiyuujinunite@gmail.com>
2021-12-19 16:58:25 +0900

    update hobbies
    - update detail
    - hobbies for pc, sp styles

--- src/data/hobbies.ts
+++ src/data/hobbies.ts
@@ -5,11 +5,30 @@ export interface HobbyItem {
 
 export const hobbies: HobbyItem[] = [
   {
-    header: 'Public',
-    items: ['Voice Communication', 'Social Networking'],
+    header: 'Communication',
+    items: ['Twitter Spaces', 'Discord', 'Clubhouse'],
   },
   {
-    header: 'Private',
-    items: ['Aviation', 'Camera'],
+    header: 'Social Networking',
+    items: ['Twitter', 'Instagram'],
+  },
+  {
+    header: 'Gadget',
+    items: [
+      'iPhone 13 Pro Max',
+      'iPhone Xs Max',
+      'iPhone 8 plus (not currently used)',
+      'iPhone 6 plus (not currently used)',
+      'iPhone 4 (not currently used)',
+      'BlackBerry (not currently used)',
+      'Pixel 6 Pro',
+      'Pixel 3a',
+      'Nikon D7300',
+      'Nikon D5300 (not currently used)',
+    ],
+  },
+  {
+    header: 'Aviation',
+    items: ['Japan Airlines', 'All Nippon Airways', 'Thai Airways'],
   },
 ]
