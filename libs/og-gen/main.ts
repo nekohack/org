b1eebb9ba7b7cd09cfcf082c40f1f7c2cd92a68d
jiyuujin <jiyuujinunite@gmail.com>
2022-01-10 13:52:12 +0900

    refactor: lib, tool and others

--- libs/og-gen/main.ts
+++ libs/og-gen/main.ts
@@ -3,9 +3,9 @@ const path = require("path");
 
 const fs = require("fs");
 
-const OG_SOURCE_DIR_PATH = path.join(__dirname, "..", "data");
+const OG_SOURCE_DIR_PATH = path.join(__dirname, "data");
 const OG_SOURCE_HTML_FILE_PATH = path.join(OG_SOURCE_DIR_PATH, "og.html");
-const OG_DIR_PATH = path.join(__dirname, "..", "apps", "nextjs", "public", "og");
+const OG_DIR_PATH = path.join(__dirname, "..", "..", "apps", "nextjs", "public", "og");
 
 async function captureOgImage(
   browser: any,
