0edc5db95109d6f84ebfc83e75db2dbec9d34f99
jiyuujin <jiyuujinunite@gmail.com>
2022-10-31 21:12:00 +0900

    test - shared-ui to storybook, update actions

---
+++ libs/shared/ui/lib/LinkPreview/index.stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/react'
+import { LinkPreview, LinkPreviewProps } from './index'
+
+export default {
+  component: LinkPreview,
+  title: 'LinkPreview',
+} as Meta
+
+const Template: Story<LinkPreviewProps> = (args) => <LinkPreview {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  title: 'Test',
+  description: 'example',
+  url: 'https://webneko.dev/',
+  image: new URL('../images/bakeneko2.png', import.meta.url).href,
+}
