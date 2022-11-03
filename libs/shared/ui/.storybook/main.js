6bbc22a6bbc5c1bdf43b214d7f54d7d8e38e1995
jiyuujin <jiyuujinunite@gmail.com>
2022-11-03 10:57:15 +0900

    fix: customize viteFinal

--- libs/shared/ui/.storybook/main.js
+++ libs/shared/ui/.storybook/main.js
@@ -1,3 +1,5 @@
+const react = require('@vitejs/plugin-react');
+
 module.exports = {
   core: {
     builder: '@storybook/builder-vite',
@@ -10,4 +12,23 @@ module.exports = {
   addons: [
     '@nrwl/react/plugins/storybook',
   ],
+
+  async viteFinal(config) {
+    config.plugins = config.plugins.filter(
+      (plugin) =>
+        !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react'))
+    );
+
+    config.plugins.push(
+      react({
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      })
+    );
+
+    return config;
+  },
 };
