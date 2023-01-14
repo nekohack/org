9d046c0c5567d7c8711056c61226f626a0282cf6
jiyuujin <jiyuujinunite@gmail.com>
2023-01-14 12:55:25 +0900

    refactor: nprogress, others
    - company-app
    - nav, footer

---
+++ apps/company-app/src/styles/nprogress.scss.d.ts
@@ -0,0 +1,6 @@
+export const bar: string
+export const nprogress: string
+export const nprogressCustomParent: string
+export const nprogressSpinner: string
+export const spinner: string
+export const spinnerIcon: string
