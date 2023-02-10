cbfb3615e2b23607eee3c8a063daa54be1748984
yuma-kitamura <jiyuujinunite@gmail.com>
2023-02-11 00:12:36 +0900

    update stories

--- libs/shared/ui/lib/Job/Job.stories.tsx
+++ libs/shared/ui/lib/Job/Job.stories.tsx
@@ -1,14 +1,28 @@
 import { Story, Meta } from '@storybook/react'
-import { Job, JobProps } from './Job'
+import { Job, JobProps } from '.'
 import data from '../../../data/lib/json/jobs.json'
 import React from 'react'
 
 export default {
   component: Job,
   title: 'Job',
-  args: {
-    data: data,
-  },
 } as Meta
 
-export const Default: Story<JobProps> = (args) => <Job {...args} />
+export const Default: Story<JobProps> = (args) => {
+  const jobs = Object.entries(data)
+    .map(([type, value]) => {
+      return value.map((v) => ({
+        type,
+        ...v,
+      }))
+    })
+    .flat()
+
+  return (
+    <Job>
+      {jobs.map((j, index) => (
+        <Job.Item key={index} {...j} description={`${j.position}: ${j.comment}`} />
+      ))}
+    </Job>
+  )
+}
