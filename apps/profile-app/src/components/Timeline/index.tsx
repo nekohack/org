9dbfe9467cb4b43362dcec5d83ee6875c8020ca4
yuma-kitamura <jiyuujinunite@gmail.com>
2023-02-10 22:18:25 +0900

    refactor: timeline

--- apps/profile-app/src/components/Timeline/index.tsx
+++ apps/profile-app/src/components/Timeline/index.tsx
@@ -1,19 +1,59 @@
+import { useState } from 'react'
+import { css } from '@emotion/react'
 import { t } from 'i18next'
-import { Timeline as TimelineTypes } from '@org/shared/types'
 import { timelines } from '@org/shared/data'
-import { Timeline, Typography } from '@org/shared/ui'
+import { CssResetButton, Icon, Timeline, Typography } from '@org/shared/ui'
 
 const TimelineSection: FC = () => {
+  const [length, setLength] = useState(7)
+  const [status, setStatus] = useState<'View more' | 'View less'>('View more')
+
+  function showMore() {
+    if (length > 7) {
+      setLength(7)
+      setStatus('View more')
+    } else {
+      setLength(() => timelines.length)
+      setStatus('View less')
+    }
+  }
+
   return (
     <section id="timeline">
       <Typography variant="subtitle 2">{t('timeline')}</Typography>
       <Typography variant="body 2">{t('timeline_description')}</Typography>
       <div className="section-wrapper">
-        <Timeline<TimelineTypes>
-          data={timelines}
-          keyObj={{ primaryLeft: 'title', primaryRight: 'date', secondary: 'description' }}
-          alternate
-        />
+        <Timeline>
+          {timelines.slice(0, length).map((t, index) => (
+            <Timeline.Card key={index} title={t.title} date={t.fullDate}>
+              <div className="text-gray-300">
+                <Typography variant="body 3">{t.description}</Typography>
+              </div>
+            </Timeline.Card>
+          ))}
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              justify-content: center;
+              button {
+                cursor: pointer;
+                background: none;
+                border: none;
+              }
+            `}
+          >
+            <CssResetButton className="text-red-300" onClick={showMore}>
+              <Typography variant="subtitle 3">{status}</Typography>
+            </CssResetButton>
+            {status === 'View more' && (
+              <div className="text-gray-300">
+                <Icon name="arrow-down" />
+              </div>
+            )}
+          </div>
+        </Timeline>
       </div>
     </section>
   )
--- libs/shared/ui/lib/Timeline/Card/index.styles.tsx
+++ libs/shared/ui/lib/Timeline/Card/index.styles.tsx
@@ -50,3 +50,59 @@ export const root = (placement: 'left' | 'right') => () =>
       max-width: 100%;
     }
   `
+
+export const item = css`
+  margin: 0.4rem 0;
+  @media (max-width: 600px) {
+    padding: 8px;
+  }
+`
+
+export const leftItem = css`
+  left: 0;
+  padding: 0 36px 12px 0;
+  &::before {
+    position: absolute;
+    top: 50%;
+    right: 30px;
+    width: 0;
+    height: 0;
+    content: '';
+    border: solid var(--secondary);
+    border-color: transparent transparent transparent var(--secondary);
+    border-width: 10px 0 10px 10px;
+    transform: translateY(-50%);
+  }
+  @media screen and (max-width: 1300px) {
+    &::after {
+      left: 8px;
+      z-index: 80;
+    }
+  }
+`
+
+export const rightItem = css`
+  left: 50%;
+  padding: 0 0 12px 36px;
+  &::after {
+    left: -12px;
+  }
+  &::before {
+    position: absolute;
+    top: 50%;
+    left: 30px;
+    width: 0;
+    height: 0;
+    content: '';
+    border: 10px solid var(--secondary);
+    border-color: transparent var(--secondary) transparent transparent;
+    border-width: 10px 10px 10px 0;
+    transform: translateY(-50%);
+  }
+  @media screen and (max-width: 1300px) {
+    &::after {
+      left: 8px;
+      z-index: 80;
+    }
+  }
+`
