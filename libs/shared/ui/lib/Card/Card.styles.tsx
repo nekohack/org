9b46fe76ed07887569bb4c1aadf8de3d90b932b5
yuma-kitamura <jiyuujinunite@gmail.com>
2023-02-15 07:32:36 +0900

    feat: Card

---
+++ libs/shared/ui/lib/Card/Card.styles.tsx
@@ -0,0 +1,61 @@
+import { css } from '@emotion/react'
+
+export const root = css`
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: #9e9e9e 0px 0px 4px;
+`
+
+export const header = css`
+  background: #312e81;
+  padding: 12px;
+  position: relative;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+`
+
+export const name = css`
+  color: #fefefe;
+`
+
+export const subheader = css`
+  color: #e2e2e2;
+`
+
+export const avatar = css`
+  position: absolute;
+  right: 12px;
+  top: 4px;
+  img {
+    height: 60px;
+    border-radius: 100%;
+  }
+`
+
+export const qrcode = css`
+  position: absolute;
+  right: 12px;
+  top: 4px;
+  img {
+    height: 60px;
+  }
+`
+
+export const content = css`
+  padding: 10px;
+  position: relative;
+`
+
+export const contentList = css`
+  list-style: none;
+  width: calc(100% - 120px);
+  padding: 0;
+  margin: 0;
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 4px;
+    color: #2d2d2d;
+  }
+`
---
+++ libs/shared/ui/lib/Card/Card.tsx
@@ -0,0 +1,73 @@
+import { ReactNode } from 'react'
+import { Icon } from '../Icon/Icon'
+import { Typography } from '../Typography/Typography'
+import * as styles from './Card.styles'
+
+export interface CardProps {
+  name: string
+  avatar: ReactNode
+  qrcode: ReactNode
+  subheader?: ReactNode
+  options: {
+    free?: ReactNode
+    star?: string
+    phone?: string
+    email?: string
+    shares?: string[][]
+    location?: string
+  }
+}
+
+export function Card({ name, avatar, qrcode, subheader, options }: CardProps) {
+  const { star, phone, email, shares, location, free } = options
+
+  return (
+    <div css={styles.root}>
+      <div css={styles.header}>
+        <div css={styles.avatar}>{avatar}</div>
+        <div css={styles.name}>
+          <Typography variant="subtitle 2">{name}</Typography>
+        </div>
+        {subheader && <div css={styles.subheader}>{subheader}</div>}
+      </div>
+      <div css={styles.content}>
+        <div css={styles.qrcode}>{qrcode}</div>
+        <ul css={styles.contentList}>
+          {star && (
+            <li>
+              <Icon name="star" />
+              <Typography variant="body 3">{star}</Typography>
+            </li>
+          )}
+          {phone && (
+            <li>
+              <Icon name="phone" />
+              <Typography variant="body 3">{phone}</Typography>
+            </li>
+          )}
+          {email && (
+            <li>
+              <Icon name="email" />
+              <Typography variant="body 3">{email}</Typography>
+            </li>
+          )}
+          {shares && shares.length > 0 && (
+            <li>
+              <Icon name="share" />{' '}
+              {shares.map(([type, text]) => (
+                <Typography variant="body 3">{text}</Typography>
+              ))}
+            </li>
+          )}
+          {location && (
+            <li>
+              <Icon name="map" />
+              <Typography variant="body 3">{location}</Typography>
+            </li>
+          )}
+          {free && <li>{free}</li>}
+        </ul>
+      </div>
+    </div>
+  )
+}
