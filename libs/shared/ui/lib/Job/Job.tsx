c7781a49c65dec1b23f1be33f5e02270c5c33850
yuma-kitamura <jiyuujinunite@gmail.com>
2023-02-15 07:49:52 +0900

    update brand color

--- libs/shared/ui/lib/Job/Job.tsx
+++ libs/shared/ui/lib/Job/Job.tsx
@@ -10,16 +10,16 @@ export function Job({ children }: React.PropsWithChildren<JobProps>) {
   return (
     <Fragment>
       <div className="flex-1 w-full overflow-hidden">
-        <div className="bg-slate-800 rounded-lg text-gray-300 shadow-lg ring-white/5 overflow-hidden">
+        <div className="bg-indigo-900 rounded-lg text-gray-200 shadow-lg ring-white/5 overflow-hidden">
           <div className="relative" style={{ height: `${totalPx}px` }}>
-            <div className="flex justify-around text-lg bg-slate-700 z-30 font-semibold text-gray-300">
+            <div className="flex justify-around text-lg bg-indigo-700 z-30 font-semibold text-gray-200">
               <Typography variant="subtitle 3">Main</Typography>
               <Typography variant="subtitle 3">Side</Typography>
             </div>
-            <div className="border-slate-700 border-l-2 absolute left-2/4 right-2/4 h-full z-10" />
+            <div className="border-indigo-700 border-l-2 absolute left-2/4 right-2/4 h-full z-10" />
             {[...Array(currentYear - minYear + 1)].map((_, i) => (
               <div
-                className="border-slate-700 border-b-2 absolute w-full text-gray-300 select-none"
+                className="border-indigo-700 border-b-2 absolute w-full text-gray-200 select-none"
                 style={{
                   bottom: `${getTotal(minYear + i + 1, 1) * monthPx}px`,
                 }}
