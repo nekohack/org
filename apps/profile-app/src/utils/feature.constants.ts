d3a2b9e320cf5e778962ee35922e35b34c298875
yuma-kitamura <jiyuujinunite@gmail.com>
2023-02-23 08:49:33 +0900

    stripe payment, others

--- apps/profile-app/src/utils/feature.constants.ts
+++ apps/profile-app/src/utils/feature.constants.ts
@@ -1,4 +1,5 @@
 export const JOB = false
-export const PAYMENT = false
+export const PAYMENT_APP = false // show app
+export const PAYMENT_API = false // true: LIVE, false: TEST
 export const SLIDE = true
 export const TIMELINE = false
---
+++ libs/shared/ui/lib/Payment/Payment.styles.tsx
@@ -0,0 +1,31 @@
+import { css } from '@emotion/react'
+
+export const root = css`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin-right: auto;
+  margin-left: auto;
+  vertical-align: middle;
+`
+
+export const checkoutWrapper = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 1em auto 0;
+  text-align: center;
+`
+
+export const checkout = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const checkoutManual = css`
+  display: block;
+  margin: 0 auto 1em;
+  text-align: center;
+`
