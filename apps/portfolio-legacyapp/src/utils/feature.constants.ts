0d77cf60557c9f823b4eaab73b76b85f7694c593
jiyuujin <jiyuujinunite@gmail.com>
2023-12-31 08:23:31 +0900

    timeline to false

--- apps/portfolio-legacyapp/src/utils/feature.constants.ts
+++ apps/portfolio-legacyapp/src/utils/feature.constants.ts
@@ -2,4 +2,4 @@ export const JOB = false
 export const PAYMENT_APP = false // show app
 export const PAYMENT_API = false // true: LIVE, false: TEST
 export const PRODUCT = false
-export const TIMELINE = true
+export const TIMELINE = false
