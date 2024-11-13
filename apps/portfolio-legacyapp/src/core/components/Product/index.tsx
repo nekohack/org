fb7edab1f511a64fa282758eb206eaf5f1c37645
jiyuujin <jiyuujinunite@gmail.com>
2024-11-13 16:48:42 +0900

    fix: mobile

--- apps/portfolio-legacyapp/src/core/components/Product/index.tsx
+++ apps/portfolio-legacyapp/src/core/components/Product/index.tsx
@@ -19,35 +19,33 @@ const ProductSection: FC = () => {
   return (
     <section className="section">
       <Typography variant="subtitle 1">{t('product')}</Typography>
-      <div className="section-wrapper">
-        <div
-          css={css`
-            display: flex;
-            flex-direction: column;
-            gap: 12px;
-          `}
-        >
-          {products
-            .filter((node) => node.enabled === true)
-            .map((node, index) => {
-              return (
-                <Card
-                  key={index}
-                  name={t(node.title)}
-                  subheader={<Typography variant="body 3">{tags(node.skills)}</Typography>}
-                  avatar={<Avatar alt="" src="https://i.imgur.com/X0CcoU9.jpg" size="large" />}
-                  qrcode={<img width={90} height={60} alt="" src={`/og/${node.image}.jpg`} />}
-                  options={{
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          gap: 12px;
+        `}
+      >
+        {products
+          .filter((node) => node.enabled === true)
+          .map((node, index) => {
+            return (
+              <Card
+                key={index}
+                name={t(node.title)}
+                subheader={<Typography variant="body 3">{tags(node.skills)}</Typography>}
+                avatar={<Avatar alt="" src="https://i.imgur.com/X0CcoU9.jpg" size="large" />}
+                qrcode={<img width={90} height={60} alt="" src={`/og/${node.image}.jpg`} />}
+                options={{
                     star: node.position,
                     urls: node.urls,
                     shares: node.shares,
                   }}
-                >
-                  <Typography variant="body 3">{t(node.description)}</Typography>
-                </Card>
-              )
-            })}
-        </div>
+              >
+                <Typography variant="body 3">{t(node.description)}</Typography>
+              </Card>
+            )
+          })}
       </div>
     </section>
   )
